e0511b7e6aebb8d2b05dc443f911d4ec0ec0c9a5
abimek <abi.mek2005@gmail.com>
2023-06-23 19:12:19 -0400

    added the threshold flag for ai operations

--- tests/study.test.ts
+++ tests/study.test.ts
@@ -4,16 +4,16 @@ const assert = require('assert');
 describe('Study', function () {
         describe('JSON.stringify()', function () {
             it('should return proper json', function () {
-                let jsonObj = new Study("Test", 0, "");
+                let jsonObj = new Study("Test", 0, "", 2);
                 assert.equal(
                     JSON.stringify(jsonObj),
-                    "{\"name\":\"Test\",\"project_uuid\":\""+jsonObj.project_uuid +"\",\"start_date\":"+jsonObj.start_date+",\"end_date\":" + jsonObj.end_date +",\"description\":\"\"}" )
+                    "{\"name\":\"Test\",\"project_uuid\":\""+jsonObj.project_uuid +"\",\"start_date\":"+jsonObj.start_date+",\"end_date\":" + jsonObj.end_date +",\"description\":\"\",\"threshold\":2}" )
             })
         });
 
         describe("JSON.parse()", function () {
             it("should return a Study object", function () {
-                let studyObj = new Study("TEST", 0,"")
+                let studyObj = new Study("TEST", 0,"", 2)
                 let studyStr = JSON.stringify(studyObj)
                 let study: Study = plainToInstance(Study,JSON.parse(studyStr) as Object);
                 assert.deepEqual(study, studyObj)
