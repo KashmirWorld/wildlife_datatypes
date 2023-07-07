d00b6d6a6572117c1736050013b3a9b8477248fb
Jviguy <jviguytwo2@gmail.com>
2023-07-06 22:56:16 -0400

    updates to study handling

--- src/study.ts
+++ src/study.ts
@@ -12,8 +12,7 @@ export class Study {
     // This field refers to the date that this studies data was updated, the data being the properties above ^^
     // this doesn't include camera_stations as they house their own lastupdated property
     public lastupdated: number
-    public camera_stations: Map<string,CameraStation>
-
+    public camera_stations: CameraStation[]
     constructor(name: string, end_date: number, description: string, threshold: number) {
         this.name = name
         this.project_uuid = uuidv4()
@@ -21,7 +20,7 @@ export class Study {
         this.end_date = end_date
         this.description = description
 		this.threshold = threshold
-        this.camera_stations = new Map<string,CameraStation>();
+        this.camera_stations = []
         this.lastupdated = this.start_date
     }
 
@@ -34,5 +33,21 @@ export class Study {
         return new Date(this.end_date * 1000)
     }
 
+    get_camera_station_by_camera_id(camera_id: string): CameraStation | null {
+        for (let camera_station of this.camera_stations) {
+            if (camera_station.camera_id == camera_id) {
+                return camera_station
+            }
+        }
+        return null
+    }
+
+    add_camera_station(camera_station: CameraStation) {
+        this.camera_stations.push(camera_station)
+    }
+
+    remove_camera_station(camera_station: CameraStation) {
+        this.camera_stations.splice(this.camera_stations.indexOf(camera_station), 1)
+    }
 }
 
