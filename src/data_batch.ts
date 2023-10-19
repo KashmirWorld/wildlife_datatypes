5222f7b4a0cb20b13bc2df4b91cc59674710cee0
DaanKwF <daan@kashmirworldfoundation.org>
2023-10-19 22:33:59 +0200

    .

--- src/data_batch.ts
+++ src/data_batch.ts
@@ -1,14 +1,15 @@
 import 'reflect-metadata';
 
 export class DataBatch {
+
     public readonly uuid: string;
     public readonly date: number;
     public readonly author: string;
     public camera_station_id: string;
     public note: string;
     public file_paths: string[];
-    public detections: { [id: string] : (Uint8Array | Float32Array | Int32Array)[] };
-    public class_detections: { [id: number] : string[] };
+    // public detections: { [id: string] : (Uint8Array | Float32Array | Int32Array)[] };
+    // public class_detections: { [id: number] : string[] };
 
     constructor(uuid: string, author: string, camera_station_id: string, note: string){
       this.uuid = uuid;
@@ -17,26 +18,26 @@ export class DataBatch {
       this.camera_station_id = camera_station_id;
       this.note = note;
       this.file_paths = [];
-      this.detections = {};
-      this.class_detections = {};
+      // this.detections = {};
+      // this.class_detections = {};
     };
 
-    get data_as_data(): Date {
-        return new Date(this.date * 1000)
-    }
+    // get data_as_data(): Date {
+    //     return new Date(this.date * 1000)
+    // }
 
-    get num_images(): number {
-      return this.file_paths.length
-    }
+    // get num_images(): number {
+    //   return this.file_paths.length
+    // }
 
-    get_images_by_detected_class(class_id: number): string[] {
-        return this.class_detections[class_id];
-    }
+    // get_images_by_detected_class(class_id: number): string[] {
+    //     return this.class_detections[class_id];
+    // }
 
-    add_detections(image_path: string, detections: (Uint8Array | Float32Array | Int32Array)[], class_IDs: number[]) {
-      this.detections = {...this.detections, image_path : detections};
-      for (const class_ID of class_IDs) {
-        this.class_detections[class_ID] = [...this.class_detections[class_ID], image_path];
-      }
-    }
+    // add_detections(image_path: string, detections: (Uint8Array | Float32Array | Int32Array)[], class_IDs: number[]) {
+    //   this.detections = {...this.detections, image_path : detections};
+    //   for (const class_ID of class_IDs) {
+    //     this.class_detections[class_ID] = [...this.class_detections[class_ID], image_path];
+    //   }
+    // }
   };
