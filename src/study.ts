acfa767666004607c637dc9fae19b4319cfe5ce9
DaanKwF <daan@kashmirworldfoundation.org>
2023-10-19 21:12:09 +0200

    .

--- src/study.ts
+++ src/study.ts
@@ -7,13 +7,13 @@ export class Study {
 
     public name: string
     public readonly project_uuid: string
-    // public readonly start_date: number
+    public readonly start_date: number
     public end_date: number
     public description: string
 	public threshold: number
     // This field refers to the date that this studies data was updated, the data being the properties above ^^
     // this doesn't include camera_stations as they house their own lastupdated property
-    // public lastupdated: number
+    public lastupdated: number
 
     @Type(() => CameraStation)
     public camera_stations: CameraStation[]
@@ -23,13 +23,13 @@ export class Study {
     constructor(uuid: string, name: string, end_date: number, description: string, threshold: number) {
         this.name = name
         this.project_uuid = uuid
-        // this.start_date = Math.floor(new Date().getTime() / 1000)
+        this.start_date = Math.floor(new Date().getTime() / 1000)
         this.end_date = end_date
         this.description = description
 		this.threshold = threshold
         this.camera_stations = []
         this.wildlife_sightings = []
-        // this.lastupdated = this.start_date
+        this.lastupdated = this.start_date
     }
 
     // get start_date_as_date(): Date {
