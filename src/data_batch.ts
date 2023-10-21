7d8012e353837b516e48839c8b1d04f87cb7387e
DaanKwF <daan@kashmirworldfoundation.org>
2023-10-21 13:57:34 +0200

    .

--- src/data_batch.ts
+++ src/data_batch.ts
@@ -8,7 +8,7 @@ export class DataBatch {
     public readonly study_name: string;
     public readonly camera_station_id: string;
     public note: string;
-    public file_paths: string[];
+    public image_IDs: string[];
     public detections: { [id: string] : (Uint8Array | Float32Array | Int32Array)[] };
     public class_detections: { [id: number] : string[] };
 
@@ -19,7 +19,7 @@ export class DataBatch {
       this.study_name = study_name;
       this.camera_station_id = camera_station_id;
       this.note = note;
-      this.file_paths = [];
+      this.image_IDs = [];
       this.detections = {};
       this.class_detections = {};
     };
@@ -29,22 +29,22 @@ export class DataBatch {
     }
 
     get_num_images(): number {
-      return this.file_paths.length
+      return this.image_IDs.length
     }
 
-    get_images_by_detected_class(class_id: number): string[] {
+    get_image_IDs_by_class(class_id: number): string[] {
         return this.class_detections[class_id];
     }
 
-    add_detections(image_path: string, detections: (Uint8Array | Float32Array | Int32Array)[], class_IDs: number[]) {
-      this.detections[image_path] = detections;
+    add_detections(image_ID: string, detections: (Uint8Array | Float32Array | Int32Array)[], class_IDs: number[]) {
+      this.detections[image_ID] = detections;
       for (const class_ID of class_IDs) {
         if (this.class_detections[class_ID]) {
-          if (!this.class_detections[class_ID].includes(image_path)) {
-            this.class_detections[class_ID] = [...this.class_detections[class_ID], image_path];
+          if (!this.class_detections[class_ID].includes(image_ID)) {
+            this.class_detections[class_ID] = [...this.class_detections[class_ID], image_ID];
           }
         } else {
-          this.class_detections[class_ID] = [image_path];
+          this.class_detections[class_ID] = [image_ID];
         }
       }
     }
